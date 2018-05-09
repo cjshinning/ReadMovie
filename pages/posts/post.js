465e8e0d73e6c1b05c01ff97c94a23cbce9cb8ac
cjshinning <784319417@qq.com>
2018-05-09 17:39:14 +0800

    小程序模板化

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,5 @@
+var postData = require('../../data/posts-data.js')
+
 Page({
 
   /**
@@ -12,77 +14,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var posts_content = [
-      {
-        date: "Sep 18 2016",
-        title: "正是虾肥蟹壮时",
-        post_img: "/images/post/crab.png",
-        avatar_img: "/images/avatar/1.png",
-        content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-        view_num: "112",
-        collect_num: "96",
-      },
-      {
-        date: "Nov 20 2016",
-        title: "比利·林恩的中场故事",
-        post_img: "/images/post/bl.png",
-        avatar_img: "/images/avatar/2.png",
-        content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
-        view_num: "62",
-        collect_num: "92",
-      }
-    ]
+    // this.data.postList = postData.postList
+
     this.setData({
-      posts_key: posts_content
+      posts_key: postData.postList
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    // console.log('onReady')
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    // console.log('onShow')
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    // console.log('onHide')
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    // console.log('onUnload')
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -7,18 +7,5 @@ Page({
     wx.redirectTo({
       url: '../posts/post'
     })
-    // console.log('execute onContainerTap')
-  },
-
-  onSubTap: function(){
-    console.log('execute onSubTap')
-  },
-
-  onUnload: function(){
-    // console.log('welcome page is onUnload')
-  },
-
-  onHide: function(){
-    // console.log('welcome page is onHide')
   }
 })
